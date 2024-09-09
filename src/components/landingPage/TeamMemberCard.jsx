ab2daa593319cb12eca3685277ea04916d771aee
Ayush Sonar <ayusht1d@gmail.com>
2024-09-09 15:13:33 +0530

    teamcard donr

--- src/components/landingPage/TeamMemberCard.jsx
+++ src/components/landingPage/TeamMemberCard.jsx
@@ -5,9 +5,11 @@ function TeamMemberCard() {
     <div className='relative w-[227px] h-[271px] bg-white ml-5'>
         <img className="absolute rounded-[3px]" src="/landingpage/membercardbg.png"></img>
         <div className='absolute px-4'>
-        <div className='w-[194px] h-[184px] bg-[#94BFFF]  mt-4 rounded-[3px] '></div>
+        <div className='w-[194px] h-[184px] bg-[#94BFFF]  mt-4 rounded-[3px] '>
+          <img className='absolute top-[-50px]' src='{imgpath}'></img>
+        </div>
         <h1 className='mt-1 text-center font-productsans text-2xl font-bold'>Shree Shinde</h1>
-        <h2 className='text-center font-productsans text-[#5E5E5E]'>UI UX Lead</h2>
+        <h2 className='text-center font-productsans text-[#5E5E5E] font-bold'>UI UX Lead</h2>
         </div>
     </div>
   )
