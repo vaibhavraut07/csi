12099e91b8095710a08db7abb3faaeb2c47f3081
D-Curse <dhruvbhatada@gmail.com>
2024-09-10 15:09:43 +0530

    Timeline Done - Added Scroll animation :(

--- src/components/bootcampPage/Timeline.jsx
+++ src/components/bootcampPage/Timeline.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 function Timeline() {
 
@@ -85,23 +85,77 @@ function Timeline() {
     },
   ]
 
+  const [visibleElements, setVisibleElements] = useState([])
+  const [animatedElements, setAnimatedElements] = useState([]);
+  const timelineRef = useRef(null)
+
+  const handleScroll = () => {
+    const timeline = timelineRef.current;
+    if (!timeline) return;
+
+    const viewportHeight = window.innerHeight;
+    const elements = [];
+
+    timeline.childNodes.forEach((child, index) => {
+      const elementRect = child.getBoundingClientRect();
+      const elementTop = elementRect.top;
+      const elementBottom = elementRect.bottom;
+
+      if (elementTop <= viewportHeight && elementBottom >= 0) {
+        elements.push(index);
+      }
+    });
+
+    setVisibleElements(elements);
+  };
+
+  useEffect(() => {
+    setAnimatedElements([])
+
+    window.addEventListener('scroll', handleScroll)
+    handleScroll()
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, []);
+
+  useEffect(() => {
+    setAnimatedElements((prevAnimatedElements) => {
+      const newAnimatedElements = new Set(prevAnimatedElements);
+      visibleElements.forEach((element) => {
+        const timeoutId = setTimeout(() => {
+          setAnimatedElements((prevAnimatedElements) => {
+            const newAnimatedElements = new Set(prevAnimatedElements);
+            newAnimatedElements.add(element);
+            return newAnimatedElements;
+          });
+        }, 2000);
+      });
+      return newAnimatedElements;
+    });
+  }, [visibleElements]);
+
+
   return(
       <div className='timeline'>
         <div className="relative w-full my-20">
           {/* Center Line */}
           <div className="absolute w-[2px] bg-black left-1/2 top-0 h-full -translate-x-1/2 opacity-[50%]"></div>
         
-          <div className="flex flex-col items-center justify-center">
-            {timelineData.map((timeline, index) => (
-              <div className="w-full sm:w-[80%]">
-                <div key={index} className="w-full flex items-center mb-8 sm:mb-12 justify-center sm:justify-none">
+          <div className="flex flex-col items-center justify-center" ref={timelineRef}>
+                      {timelineData.map((timeline, index) => (
+              <div key={timeline.date} className="w-full sm:w-[80%]">
+                <div className="w-full flex items-center mb-8 sm:mb-12 justify-center sm:justify-none">
                   {index % 2 === 0 ? (
                     <>
                       {/* Left Side (Title) */}
-                      <div className="w-1/2 sm:w-1/3 flex justify-end px-4 sm:px-8 animate-fadeInRight">
+                      <div className={`w-1/2 sm:w-1/3 flex justify-end px-4 sm:px-8 ${
+                        visibleElements.includes(index) && !animatedElements.has(index)
+                          ? 'animate-fadeInLeft'
+                          : ''
+                      }`}>
                         <div className="border-2 rounded-[20px] p-4 sm:p-8 w-full">
-                          {timeline.events.map((event, index) => (
-                            <div key={index} className="flex items-center justify-between my-4 sm:my-2 flex-col sm:flex-row">
+                          {timeline.events.map((event, eventIndex) => (
+                            <div key={event.title + eventIndex} className="flex items-center justify-between my-4 sm:my-2 flex-col sm:flex-row">
                               <div className="flex w-full flex-col sm:flex-row justify-start">
                                 <img src={event.icon} alt={event.title} className='w-6 sm:w-8 h-6 pr-0 sm:pr-2' />
                                 <h3 className="text-base sm:text-xl font-semibold">{event.title}</h3>
@@ -116,7 +170,11 @@ function Timeline() {
                       <img src="/bootcamppage/timeline/circle.png" alt="" className="w-8 sm:w-12" />
 
                       {/* Right Side (Date) */}
-                      <div className="w-1/2 sm:w-1/3 flex justify-start px-4 sm:px-8 animate-fadeInLeft">
+                      <div className={`w-1/2 sm:w-1/3 flex justify-start px-4 sm:px-8 ${
+                        visibleElements.includes(index) && !animatedElements.has(index)
+                          ? 'animate-fadeInRight'
+                          : ''
+                      }`}>
                         <div className="w-full sm:w-[65%] text-center font-bold bg-[#F0F0F0] p-4 rounded-[25px] text-base sm:text-2xl">
                           {timeline.date}
                         </div>
@@ -125,7 +183,11 @@ function Timeline() {
                   ) : (
                     <>
                       {/* Left Side (Date) */}
-                      <div className="w-1/2 sm:w-1/3 flex justify-end px-4 sm:px-8 animate-fadeInLeft">
+                      <div className={`w-1/2 sm:w-1/3 flex justify-end px-4 sm:px-8 ${
+                        visibleElements.includes(index) && !animatedElements.has(index)
+                          ? 'animate-fadeInLeft'
+                          : ''
+                      }`}>
                         <div className="w-full sm:w-[65%] text-center font-bold bg-[#F0F0F0] p-4 rounded-[25px] text-base sm:text-2xl">
                           {timeline.date}
                         </div>
@@ -135,10 +197,14 @@ function Timeline() {
                       <img src="/bootcamppage/timeline/circle.png" alt="" className="w-8 sm:w-12" />
 
                       {/* Right Side (Title) */}
-                      <div className="w-1/2 sm:w-1/3 flex justify-start px-4 sm:px-8 animate-fadeInRight">
+                      <div className={`w-1/2 sm:w-1/3 flex justify-start px-4 sm:px-8 ${
+                        visibleElements.includes(index) && !animatedElements.has(index)
+                          ? 'animate-fadeInRight'
+                          : ''
+                      }`}>
                         <div className="border-2 rounded-[20px] p-4 sm:p-8 w-full">
-                          {timeline.events.map((event, index) => (
-                            <div key={index} className="flex items-center justify-between my-4 sm:my-2 flex-col sm:flex-row">
+                          {timeline.events.map((event, eventIndex) => (
+                            <div key={event.title + eventIndex} className="flex items-center justify-between my-4 sm:my-2 flex-col sm:flex-row">
                               <div className="flex w-full flex-col sm:flex-row justify-start">
                                 <img src={event.icon} alt={event.title} className='w-6 sm:w-8 h-6 pr-0 sm:pr-2' />
                                 <h3 className="text-base sm:text-xl font-semibold">{event.title}</h3>
